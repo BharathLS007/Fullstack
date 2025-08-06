52bd190a7a1b24076f7b50f735410389eda11bb5
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-08-06 13:08:02 +0530

    navbar

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-
+import Contact from './Pages/Contact';
+import About from './Pages/About';
+import Cart from './Pages/Cart';
+import Signup from './Pages/Signup';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='About' element={<About/>}/>
+        <Route path='Contact' element={<Contact/>}/>
+        <Route path='Cart' element={<Cart/>}/>
+        <Route path='SignUp' element={<Signup/>}/>
       </Routes>
     </BrowserRouter>
   );
