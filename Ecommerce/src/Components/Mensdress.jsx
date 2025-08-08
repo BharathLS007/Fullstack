90dc0d529916af0263199329b6a7523ea3d5d937
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-08-08 13:50:43 +0530

    nav left

---
+++ Ecommerce/src/Components/Mensdress.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mensdress() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Mensdress
--- Ecommerce/src/Pages/Home.jsx
+++ Ecommerce/src/Pages/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div>
         <Navbar/>
-      <h1>This is Home page</h1>
-    </div>
+      
+      <div className="leftside-nav" > 
+         <div className="nav-links"><Link to="Mensdress">Mensdress</Link></div>
+         <div className="nav-links"><Link to="Womensdress">Womensdress</Link></div>
+         <div className="nav-links"><Link to="Toys">Toys</Link></div>
+         <div className="nav-links"><Link to="Electronics">Electronics</Link></div>
+         <div className="nav-links"><Link to="#">Medicine</Link></div>
+         <div className="nav-links"><Link to="#">Sport& Outdoor</Link></div>
+         <div className="nav-links"><Link to="#">Baby's & Toys</Link></div>
+         <div className="nav-links"><Link to="#">Groceries& pets</Link></div>
+         <div className="nav-links"><Link to="#">Health & Beauty</Link></div>
+      </div>
+ 
+     </div>
   )
 }
 
