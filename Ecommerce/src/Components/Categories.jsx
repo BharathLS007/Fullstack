b2550f296585090146e6baf9dd5ddaf335f51b1d
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-08-13 11:52:14 +0530

    watch

--- Ecommerce/src/Components/Categories.jsx
+++ Ecommerce/src/Components/Categories.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import watch from '../images/watch.jpg'
 function Categories() {
   return (
     <div>
@@ -11,8 +11,29 @@ function Categories() {
          <div className="container">
            <h2 className='Countdown'>Browse By Category</h2>
          </div>       
-         
-    </div>
+          <div className="container">
+             <div className="icons">
+               <div className="boxicon">
+                 <li className='items'><i className="fa-solid fa-mobile-screen"></i></li>
+               </div>
+               <div className="boxicon">
+                 <li className='items'><i className="fa-solid fa-headphones" ></i></li> 
+               </div>
+               <div className="boxicon">
+                 <li className='items'><i className="fa-solid fa-computer"></i></li> 
+               </div>
+               <div className="boxicon">
+                 <li className='items'><i className="fa-solid fa-watch-smart"></i></li> 
+               </div>
+               <div className="boxicon">
+                 <li className='items'><i className="fa-solid fa-gamepad"></i></li> 
+               </div>
+               <div className="boxicon">
+                 <li className='items'><img src={watch} alt="watchimg" style={{width:"20%", height:"20%"}}/></li> 
+               </div>
+             </div>
+           </div>
+           </div>
   )
 }
 
