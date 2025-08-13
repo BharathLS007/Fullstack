d8edfade2607d1a017ed873f8963bbf90e1a1396
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-08-13 12:57:22 +0530

    cat

--- Ecommerce/src/Components/Categories.jsx
+++ Ecommerce/src/Components/Categories.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import watch from '../images/watch.jpg'
-import camera from '../images/camera.jpg'
-import phone from '../images/phone.jpg'
-import headset from '../images/headset.jpg'
+import watch from '../images/watch.png'
+import camera from '../images/camera.png'
+import phone from '../images/phone.png'
+import headset from '../images/headset.png'
 import Gamepad from '../images/Gamepad.png'
 import pc from '../images/pc.png'
 function Categories() {
@@ -17,26 +17,45 @@ function Categories() {
            <h2 className='Countdown'>Browse By Category</h2>
          </div>       
           <div className="container">
-             <div className="icons">
-               <div className="boxicon">
-                 <li className='items'><img src={phone} alt="watchimg" style={{width:"100px", height:"100px%"}}/></li> 
-               </div>
-               <div className="boxicon">
-                 <li className='items'><img src={headset} alt="watchimg" style={{width:"100px", height:"100px%"}}/></li> 
-               </div>
-               <div className="boxicon">
-                 <li className='items'><img src={Gamepad} alt="watchimg" style={{width:"100px", height:"100px%"}}/></li> 
-               </div>
-               <div className="boxicon">
-                 <li className='items'><img src={camera} alt="watchimg" style={{width:"100px", height:"100px%"}}/></li> 
-               </div>
-               <div className="boxicon">
-                 <li className='items'><img src={pc} alt="watchimg" style={{width:"100px", height:"100px%"}}/></li> 
-               </div>
-               <div className="boxicon">
-                 <li className='items'><img src={watch} alt="watchimg" style={{width:"100px", height:"100px%"}}/></li> 
-               </div>
-             </div>
+            <div className="icons">
+              <div className="boxicon">
+    <li className='items'>
+      <img src={phone} alt="Phone" style={{width:"100px", height:"100px"}}/>
+      <p className="label">Phone</p>
+    </li> 
+              </div>
+              <div className="boxicon">
+                <li className='items'>
+      <img src={headset} alt="Headset" style={{width:"100px", height:"100px"}}/>
+      <p className="label">Headset</p>
+                </li> 
+              </div>
+              <div className="boxicon">
+                <li className='items'>
+      <img src={Gamepad} alt="Gamepad" style={{width:"100px", height:"100px"}}/>
+      <p className="label">Gamepad</p>
+                </li> 
+              </div>
+              <div className="boxicon">
+                <li className='items'>
+                 <i className="fa-solid fa-camera" style={{ fontSize: "100px", color: "black" }}></i>
+                  <p className="label">Camera</p>
+                </li> 
+              </div>
+              <div className="boxicon">
+    <li className='items'>
+      <img src={pc} alt="PC" style={{width:"100px", height:"100px"}}/>
+      <p className="label">PC</p>
+    </li> 
+              </div>
+              <div className="boxicon">
+    <li className='items'>
+      <img src={watch} alt="Watch" style={{width:"100px", height:"100px"}}/>
+      <p className="label">Watch</p>
+    </li> 
+              </div>
+            </div>
+
            </div>
            </div>
   )
