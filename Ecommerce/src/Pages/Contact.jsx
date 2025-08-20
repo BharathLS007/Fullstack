1905160eac4f9f6e8e43910695a07daf046a5114
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-08-20 13:04:39 +0530

    contact page process on going

--- Ecommerce/src/Pages/Contact.jsx
+++ Ecommerce/src/Pages/Contact.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
 function Contact() {
   return (
     <div>
       <Navbar/>
-      <h1>Contact Section</h1>
+      <div className="third-nav">
+        <Link to="/" className='contacthome'>Home </Link>
+        <p className='space'>/</p>
+        <Link to="/Contact" className='Contactcontact'>Contact</Link>
+      </div>
     </div>
   )
 }
