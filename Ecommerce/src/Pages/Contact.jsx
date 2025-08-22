8148d7207ed74100646cbd34aa78723481fbd18c
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-08-22 12:14:01 +0530

    form

--- Ecommerce/src/Pages/Contact.jsx
+++ Ecommerce/src/Pages/Contact.jsx
@@ -15,20 +15,20 @@ function Contact() {
             <div className="info-box">
                <div className="infobox1">
                    <div className="infocontent">
-                       <li className='call'><i class="fa-solid fa-phone"></i></li>
-                       <p>Call To Us</p>
+                       <li className='call'><i class="fa-solid fa-phone"></i></li><br /><br />
+                       <p style={{marginTop:'5px', marginLeft:'10px'}}>Call To Us</p><br /><br />
                    </div>
-                   <p>We are Avaiable 24/7,days a week,</p>
+                   <p>We are Avaiable 24/7,days a week,</p><br />
                    <p>Phone:+91 9876543210</p>
                </div>
                  <hr style={{ border: "1px solid black", margin: "10px 0",  width:"400px", paddingLeft:'10px'}} />
                <div className="infobox1">
                    <div className="infocontent">
                        <li className='call'><i class="fa-solid fa-message"></i></li>
-                       <p>Write To Us</p>
-                   </div>
-                   <p>Fill out our form and we will contact</p>
-                   <p>you within 24hours.</p>
+                       <p style={{marginTop:'5px',marginLeft:'10px'}}>Write To Us</p>
+                   </div><br />
+                   <p>Fill out our form and we will contact</p><br />
+                   <p>you within 24hours.</p><br />
                    <p>Email:abc@customersupprot.com</p>
                </div>               
             </div>
@@ -36,10 +36,10 @@ function Contact() {
               <form action="submit">
                    <div className="form-conatiner">
                  <div className="form-group">
-                  <input type="text" name='name' placeholder='your Name' required style={{width:'250px', height:'40px', paddingLeft:'10px'}}/>
+                  <input type="text" name='name' placeholder='your Name' required style={{width:'250px', height:'40px', paddingLeft:'10px', borderRadius:'0.5rem'}}/>
                  </div>
                   <div className="form-group">
-                  <input type="text" name='email' placeholder='Your Email ' required style={{width:'250px', height:'40px', paddingLeft:'10px' }}/>
+                  <input type="text" name='email' placeholder='Your Email ' required style={{width:'250px', height:'40px', paddingLeft:'10px',borderRadius:'0.5rem' }}/>
                  </div>
                  <div className="form-group">
                   <input
@@ -49,7 +49,7 @@ function Contact() {
                     maxLength="15"
                     name="phone"
                     placeholder="Your Phone"
-                    required style={{width:'250px', height:'40px', paddingLeft:'10px'}}
+                    required style={{width:'250px', height:'40px', paddingLeft:'10px',borderRadius:'0.5rem'}}
                     onInput={(e) => {
                       e.target.value = e.target.value.replace(/\D/g, '');
                     }}
@@ -58,8 +58,9 @@ function Contact() {
                   </div>                    
                     </div> 
                   <div className="form-group-full-width">
-                    <input type="text" name='message' placeholder='message' style={{width:'800px', height:'300px',paddingLeft:'30px'}}/>
+                    <input type="text" name='message' placeholder='message' style={{width:'800px', height:'200px',paddingLeft:'30px',borderRadius:'0.5rem'}}/>
                   </div>
+                  <button type="submit" style={{width:'200px', marginLeft:'70%',backgroundColor:'var(--hover)'}}>Send message</button>
               </form>
             </div>
          </div>
