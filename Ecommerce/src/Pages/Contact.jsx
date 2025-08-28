1456dd047aa5789574cdc3e38a7c0688fac98f99
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-08-28 19:13:24 +0530

    Contact

--- Ecommerce/src/Pages/Contact.jsx
+++ Ecommerce/src/Pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
 function Contact() {
   return (
     <div>
@@ -64,7 +65,10 @@ function Contact() {
               </form>
             </div>
          </div>
-
+       <div className="bottom" style={{marginTop:'155px'}}>
+                 <Footer/>
+       </div>
+    
     </div>
   )
 }
