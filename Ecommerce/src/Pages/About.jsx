513c883a351b57cacab3940c0d901ee4fd17f153
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-08-29 14:30:00 +0530

    img

--- Ecommerce/src/Pages/About.jsx
+++ Ecommerce/src/Pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
+import twins from '../images/twins.png'
 function About() {
   return (
     <div>
@@ -11,7 +12,19 @@ function About() {
                     <Link to="/About" className='Contactcontact'>About</Link>
                   </div>
 
-                  
+                 <div className="about-container">
+                    <div className="about-content-right">
+                      <h1>Our Story</h1>
+                      <div className="content-right">
+                        <p className='story'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A nemo veniam, dignissimos voluptates vero nesciunt repellendus perspiciatis in ipsum iste eius tenetur consectetur earum accusantium quibusdam doloremque architecto, culpa dolorum!
+                        Incidunt corrupti expedita mollitia officia facere provident nulla, molestias vel rem animi sed nihil minima eum voluptas quo iste praesentium. Excepturi, sint consequuntur? Eum ratione est quisquam magnam, explicabo iusto.
+                        Sequi nam odit nesciunt ut laudantium? Magni odit ipsam sequi impedit sapiente assumenda non facilis sint quam animi laborum tenetur, quia possimus atque culpa repellat cum sit numquam blanditiis temporibus?</p>
+                      </div>
+                      <div className="content-left">
+                        <img src={twins} alt="img" />
+                      </div>
+                    </div>
+                 </div>
     </div>
   )
 }
